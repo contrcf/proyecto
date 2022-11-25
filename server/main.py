77cce50f2ad58afe3b19fac4a81739322f7dcb0b
contrcf <contrf@cat.comqa>
2022-11-25 13:08:16 -0600

    dsff

--- server/main.py
+++ server/main.py
@@ -1,10 +1,9 @@
 import json
 import logging
 from fastapi import FastAPI
-from models.models import Passenger
+from server.models.models import Passenger
 from starlette.responses import JSONResponse
-from predictor.passenger_predictor import PassengerPredictor as PassengerPredictor
-
+from server.predictor.passenger_predictor import PassengerPredictor as PassengerPredictor
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -12,9 +11,7 @@ formatter = logging.Formatter("%(levelname)s: %(asctime)s|%(name)s|%(message)s")
 file_handler = logging.FileHandler("server.log")
 file_handler.setFormatter(formatter)
 
-
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
--- server/predictor/passenger_predictor.py
+++ server/predictor/passenger_predictor.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from models.models import Passenger
+from server.models.models import Passenger
 import joblib
 import config
 
